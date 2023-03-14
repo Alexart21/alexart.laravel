5377fbaacb0648edf195bd6d66c5f63f3f460bbf
Alexart21 <iskander.m.211@gmail.com>
2023-03-14 08:31:50 +0300

    добавлена задача cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,19 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Chat;
+use App\Models\Content;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
+    protected function scheduleTimezone()
+    {
+        return 'Europe/Moscow';
+    }
+
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +26,34 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            // код, выполняемый каждый день в 09:30
+            // обновляем updated_at для заголовка LastModified
+            $pages = Content::all();
+            try{
+                DB::beginTransaction();
+                foreach ($pages as $page) {
+                    $time = time() - rand(60, 600); // разброс от 1 до 10 минут
+                    // mysql сама формирует дату в поле timestamp
+                    $page->updated_at = $time;
+                    $page->save();
+                }
+                DB::commit();
+                Cache::flush();
+            }catch (Exception $e){
+                DB::rollBack();
+//                dd($e->getMessage());
+            }
+
+        })->dailyAt('09:30');
+
+        /*$schedule->call(function () {
+            // код, выполняемый раз в 5 минут
+            $chat = new Chat();
+            $chat->name = 'cron';
+            $chat->msg = 'cron-test';
+            $chat->save();
+        })->everyFiveMinutes();*/
     }
 
     /**
